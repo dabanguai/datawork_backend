8ce5380e4f1d3032a4c178915fa938ecc1c35f1e
dabanguai <1596815312@qq.com>
2025-02-21 22:58:46 +0800

    init

---
+++ src/test/java/com/dbg/datawork/MainApplicationTests.java
@@ -0,0 +1,19 @@
+package com.dbg.datawork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ *
+ * @author <a href="https://github.com/lidbg">程序员鱼皮</a>
+ * @from <a href="https://dbg.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
